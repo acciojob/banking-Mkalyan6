798b2b4f98308167533b04c429d9ba93e8a7d9ba
Mkalyan6 <120394363+Mkalyan6@users.noreply.github.com>
2023-09-10 21:36:53 +0530

    added

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -1,19 +1,90 @@
 package com.driver;
+import java.lang.Exception;
 
-public class CurrentAccount extends BankAccount{
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class CurrentAccount extends BankAccount {
     String tradeLicenseId; //consists of Uppercase English characters only
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
-        // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
+        super(name, balance, 5000);
+       tradeLicenseId=tradeLicenseId.toUpperCase();
 
+        if (balance < 5000) {
+            throw new Exception("Insufficient Balance");
+        }
+        this.tradeLicenseId = tradeLicenseId;
     }
 
     public void validateLicenseId() throws Exception {
+        if (IsValid(tradeLicenseId)) return;
         // A trade license Id is said to be valid if no two consecutive characters are same
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
+        HashMap<Character, Integer> hm = new HashMap<>();
+        //first count the frequency of characters present in id
+        for (char ch : tradeLicenseId.toCharArray()) {
+            int count = hm.getOrDefault(ch, 0) + 1;
+            hm.put(ch, count);
+        }
 
+        // Traverse through hashmap and find the failure condition where valid license id is not possible
+        for (Map.Entry<Character, Integer> mapElement : hm.entrySet()) {
+            int FreqOfChar = mapElement.getValue();
+            int FreqOfRemainingChar = tradeLicenseId.length() - FreqOfChar;
+            if (FreqOfRemainingChar < FreqOfChar - 1)
+                throw new Exception("Valid License can not be generated");
+        }
+        PriorityQueue<pair> pq = new PriorityQueue<>((a, b) -> {
+            return b.freq - a.freq;
+        });
+
+
+        // traverse through hashmap and add key value as pair in priority queue
+        for (Map.Entry<Character, Integer> mapElement : hm.entrySet()) {
+            pq.add(new pair(mapElement.getKey(), mapElement.getValue()));
+        }
+
+        StringBuilder identity = new StringBuilder();
+        while (pq.size() > 0) {
+            pair first = pq.remove();
+            // add the most frequent ocuring char to new idString and decrement frequency
+            identity.append(first.letter);
+            --first.freq;
+            if (pq.size() > 0) {
+                pair second = pq.remove();
+                // add the char to identity string  nd decrement the frequency
+                 identity.append(second.letter);
+                --second.freq;
+
+                if (first.freq > 0) pq.add(first);
+                if (second.freq > 0) pq.add(second);
+            }
+        }
+        tradeLicenseId = identity.toString();
+        System.out.println(tradeLicenseId);
     }
 
+
+    private boolean IsValid(String id) {
+       for(int i=0;i<tradeLicenseId.length()-1;i++){
+           if(tradeLicenseId.charAt(i)==tradeLicenseId.charAt(i+1)){
+               return false;
+           }
+       }
+        return true;
+
+    }
+    public static class pair{
+        char letter;
+        int freq;
+
+        public pair(char letter, int freq) {
+            this.letter = letter;
+            this.freq = freq;
+        }
+    }
 }
--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -1,10 +1,15 @@
 package com.driver;
+import java.lang.Exception;
+
 
 public class SavingsAccount extends BankAccount{
     double rate;
     double maxWithdrawalLimit;
 
     public SavingsAccount(String name, double balance, double maxWithdrawalLimit, double rate) {
+        super(name,balance,0);
+        this.rate=rate;
+        this.maxWithdrawalLimit=maxWithdrawalLimit;
         // minimum balance is 0 by default
 
     }
@@ -12,17 +17,25 @@ public class SavingsAccount extends BankAccount{
         // Might throw the following errors:
         // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
         // 2. "Insufficient Balance" : If the amount exceeds balance
+        if(amount>maxWithdrawalLimit){
+            throw new Exception("Maximum Withdraw Limit Exceed");
+        }else if(amount>getBalance()){
+            throw new Exception(("Insufficient Balance"));
+        }
+         this.setBalance(getBalance()-amount);
 
     }
 
     public double getSimpleInterest(int years){
         // Return the final amount considering that bank gives simple interest on current amount
+        double interest=(getBalance()*years*rate)/100;
+        return getBalance()+interest;
 
     }
 
     public double getCompoundInterest(int times, int years){
         // Return the final amount considering that bank gives compound interest on current amount given times per year
-
+        return getBalance()*Math.pow((1+((rate/100)/times)), times*years);
     }
 
 }
